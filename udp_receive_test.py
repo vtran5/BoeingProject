099799e98caa9367307aa25c08c8fc92643953a1
Vu Tran <vtran5@villanova.edu>
2018-10-08 16:26:33 -0400

    change udp receive

--- udp_receive_test.py
+++ udp_receive_test.py
@@ -1,6 +1,8 @@
 import socket
+import time
+from struct import*
 
-UDP_IP = "127.0.0.1"
+UDP_IP = "192.168.1.3"
 UDP_PORT = 11000
 
 sock = socket.socket(socket.AF_INET, # Internet
@@ -9,4 +11,5 @@ sock.bind((UDP_IP, UDP_PORT))
 
 while True:
     data, addr = sock.recvfrom(1024) # buffer size is 1024 bytes
+    TestData = unpack('h',data)
     print ("received message:", data)
